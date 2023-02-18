4dfd195b37d7f00a4c9eeaa124daaa1b0a46d8f9
fry42guy <47529295+fry42guy@users.noreply.github.com>
2023-02-18 12:12:58 -0600

    first commit

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,52 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+
+public final int FL_Drive_ID = 30;
+public final int FR_Drive_ID = 31;
+public final int RL_Drive_ID = 32;
+public final int RR_Drive_ID = 33;
+public final double Drive_Speed_Max = 1.0;
+
+public final int Intake_Right_ID = 40;
+public final int Intake_Left_ID = 41;
+public final double Intake_speed_Limit = 1.0; 
+
+public final int Spark_Arm_1_ID = 50;
+public final int Spark_Arm_2_ID = 51;
+public final double Arm_Speed_Max = .5;
+
+public final int PCM_ID = 1;
+
+public final int Pivot_Open = 1; 
+public final int Pivot_Close = 2; 
+
+public final int Vertical_up = 3; 
+public final int Vertical_down = 4; 
+
+public final int Clawn_Open = 5; 
+public final int Clawn_Close = 6; 
+
+
+
+
+
+
+
+
+
+
+
+}
